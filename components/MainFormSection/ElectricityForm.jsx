57ebe288aab935fcf748c0fa35076b60256df7cc
yuchiko <iyuchiko@gmail.com>
2022-07-09 12:48:52 +0300

    fix main form tabs

---
+++ components/MainFormSection/ElectricityForm.jsx
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import cx from "classnames";
+
+const ElecticityForm = ({ isActive }) => {
+  return (
+    <div
+      className={cx("tab-item", {
+        ["is-active"]: isActive
+      })}
+    >
+      <form id="electricity-form">
+        <table className="main-form__table">
+          <tbody>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="ff_company_name">
+                  1. Назва юридичної особи / код €ДРПОУ
+                </label>
+              </td>
+              <td>
+                <input
+                  id="ff_company_name"
+                  name="company_name"
+                  className="volt-input"
+                  type="text"
+                  placeholder="Ваша назва"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label
+                  className="volt-form-label"
+                  htmlFor="ff_consumption_class"
+                >
+                  2. Клас Споживання
+                </label>
+              </td>
+              <td>
+                <input
+                  id="ff_consumption_class"
+                  name="consumption_class"
+                  className="volt-input"
+                  type="text"
+                  placeholder="1 клас / 11 клас"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span className="volt-form-label">3. Група Споживання:</span>
+              </td>
+              <td>
+                <div className="d-flex">
+                  <div className="volt-form-radio">
+                    <input
+                      type="radio"
+                      id="consumption_group_a"
+                      name="radio-group"
+                    />
+                    <label htmlFor="consumption_group_a">Група А</label>
+                  </div>
+                  <div className="volt-form-radio">
+                    <input
+                      type="radio"
+                      id="consumption_group_b"
+                      name="radio-group"
+                    />
+                    <label htmlFor="consumption_group_b">Група Б</label>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="ff_address">
+                  4. Мiсце пiдключення
+                </label>
+              </td>
+              <td>
+                <input
+                  id="ff_address"
+                  name="connection_address"
+                  className="volt-input"
+                  type="text"
+                  placeholder="Область, регiон, населений пункт, адреса"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="who_is_provider">
+                  5. Хто є Постачальником електроенергiї
+                </label>
+              </td>
+              <td>
+                <input
+                  id="who_is_provider"
+                  name="who_is_provider"
+                  className="volt-input"
+                  type="text"
+                  placeholder="Назва постачальника"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="who_is_distributor">
+                  6. Хто е Розподiльником електроенергії
+                </label>
+              </td>
+              <td>
+                <input
+                  id="who_is_distributor"
+                  name="who_is_distributor"
+                  className="volt-input"
+                  type="text"
+                  placeholder="Назва розподiльника"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="price_for_3_month">
+                  7. Вартiсть 1 кВт/год за останнi 3 (три) мiсяцi, на момент
+                  подання iнформації
+                </label>
+              </td>
+              <td>
+                <input
+                  id="price_for_3_month"
+                  name="price_for_3_month"
+                  className="volt-input"
+                  type="text"
+                  placeholder="Вартість в гривні"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label className="volt-form-label" htmlFor="schedulle_payment">
+                  8. Графiк оплати за використану е/е:
+                </label>
+              </td>
+              <td>
+                <div className="grid-x grid-margin-x">
+                  <div className="cell medium-6">
+                    <input
+                      id="schedulle_payment"
+                      name="schedulle_payment"
+                      className="volt-input"
+                      type="text"
+                      placeholder="Дiючий графiк"
+                    />
+                  </div>
+                  <div className="cell medium-6">
+                    <input
+                      id="schedulle_payment_new"
+                      name="schedulle_payment_new"
+                      className="volt-input"
+                      type="text"
+                      placeholder="Бажаний графiк"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="main-form__footer">
+          <div className="d-flex flex-center">
+            <div className="volt-form-checkbox">
+              <input type="checkbox" id="check_personal_data" />
+              <label htmlFor="check_personal_data">
+                <div className="tick"></div>
+              </label>
+            </div>
+            <label
+              htmlFor="check_personal_data"
+              className="volt-form-label c-pointer"
+            >
+              Погоджуюсь на обробку персональних даних
+            </label>
+          </div>
+          <button
+            className="main-form__submit volt-button volt-button--large"
+            type="submit"
+          >
+            Розрахувати комерційну пропозицію
+          </button>
+          <p className="main-form__small-text">
+            За додатковою інформацією звертайтесь до
+            <a href="#">відділу продажів</a> або в <a href="#">FAQ</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ElecticityForm;
--- components/MainFormSection/index.jsx
+++ components/MainFormSection/index.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
+import cx from "classnames";
+
 import EnergyIcon from "images/icons/energy.svg";
 import GasIcon from "images/icons/gas.svg";
 import VoltsLogo from "images/icons/logo_shadow.svg";
+import ElecticityForm from "./ElectricityForm";
+import GasForm from "./GasForm";
 
 const MainFormSection = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabClicked = (tabIndex) => {
+    setActiveTab(tabIndex);
+  };
+
   return (
     <section className="sc main-form sc--invert sc-pt-0">
       <div className="grid-container">
         <div className="main-form__tabs">
           <div
-            className="main-form__tabs-item is-active js-form-tab-item"
-            data-main-form="electricity-form"
+            className={cx("main-form__tabs-item", {
+              ["is-active"]: activeTab === 0,
+            })}
+            onClick={() => {tabClicked(0)}}
           >
             <EnergyIcon className="services-item__icon" />
             <span>Електроенергiя</span>
           </div>
           <div
-            className="main-form__tabs-item js-form-tab-item"
-            data-main-form="gas-form"
+            className={cx("main-form__tabs-item", {
+              ["is-active"]: activeTab === 1,
+            })}
+            onClick={() => {tabClicked(1)}}
           >
             <GasIcon className="services-item__icon" />
             <span>Газ</span>
@@ -37,403 +51,8 @@ const MainFormSection = () => {
               комерційної пропозіції
             </p>
           </header>
-          <form id="electricity-form" className="js-main-form-variant">
-            <table className="main-form__table">
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="ff_company_name">
-                    1. Назва юридичної особи / код €ДРПОУ
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_company_name"
-                    name="company_name"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Ваша назва"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="ff_consumption_class"
-                  >
-                    2. Клас Споживання
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_consumption_class"
-                    name="consumption_class"
-                    className="volt-input"
-                    type="text"
-                    placeholder="1 клас / 11 клас"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span
-                    className="volt-form-label"
-                    htmlFor="ff_consumption_class"
-                  >
-                    3. Група Споживання:
-                  </span>
-                </td>
-                <td>
-                  <div className="d-flex">
-                    <div className="volt-form-radio">
-                      <input
-                        type="radio"
-                        id="consumption_group_a"
-                        name="radio-group"
-                      />
-                      <label htmlFor="consumption_group_a">Група А</label>
-                    </div>
-                    <div className="volt-form-radio">
-                      <input
-                        type="radio"
-                        id="consumption_group_b"
-                        name="radio-group"
-                      />
-                      <label htmlFor="consumption_group_b">Група Б</label>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="ff_address">
-                    4. Мiсце пiдключення
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_address"
-                    name="connection_address"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Область, регiон, населений пункт, адреса"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="who_is_provider">
-                    5. Хто є Постачальником електроенергiї
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="who_is_provider"
-                    name="who_is_provider"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Назва постачальника"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="who_is_distributor"
-                  >
-                    6. Хто е Розподiльником електроенергії
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="who_is_distributor"
-                    name="who_is_distributor"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Назва розподiльника"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="price_for_3_month"
-                  >
-                    7. Вартiсть 1 кВт/год за останнi 3 (три) мiсяцi, на момент
-                    подання iнформації
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="price_for_3_month"
-                    name="price_for_3_month"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Вартість в гривні"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="schedulle_payment"
-                  >
-                    8. Графiк оплати за використану е/е:
-                  </label>
-                </td>
-                <td>
-                  <div className="grid-x grid-margin-x">
-                    <div className="cell medium-6">
-                      <input
-                        id="schedulle_payment"
-                        name="schedulle_payment"
-                        className="volt-input"
-                        type="text"
-                        placeholder="Дiючий графiк"
-                      />
-                    </div>
-                    <div className="cell medium-6">
-                      <input
-                        id="schedulle_payment_new"
-                        name="schedulle_payment_new"
-                        className="volt-input"
-                        type="text"
-                        placeholder="Бажаний графiк"
-                      />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </table>
-            <div className="main-form__footer">
-              <div className="d-flex flex-center">
-                <div className="volt-form-checkbox">
-                  <input type="checkbox" id="check_personal_data" />
-                  <label htmlFor="check_personal_data">
-                    <div className="tick"></div>
-                  </label>
-                </div>
-                <label
-                  htmlFor="check_personal_data"
-                  className="volt-form-label c-pointer"
-                >
-                  Погоджуюсь на обробку персональних даних
-                </label>
-              </div>
-              <button
-                className="main-form__submit volt-button volt-button--large"
-                type="submit"
-              >
-                Розрахувати комерційну пропозицію
-              </button>
-              <p className="main-form__small-text">
-                За додатковою інформацією звертайтесь до
-                <a href="#">відділу продажів</a> або в <a href="#">FAQ</a>
-              </p>
-            </div>
-          </form>
-          <form id="gas-form" className="hide js-main-form-variant">
-            <table className="main-form__table">
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="ff_company_name">
-                    {" "}
-                    ГАЗ ГАЗ ГАЗ 1. Назва юридичної особи / код €ДРПОУ
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_company_name"
-                    name="company_name"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Ваша назва"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="ff_consumption_class"
-                  >
-                    2. Клас Споживання
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_consumption_class"
-                    name="consumption_class"
-                    className="volt-input"
-                    type="text"
-                    placeholder="1 клас / 11 клас"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <span
-                    className="volt-form-label"
-                    htmlFor="ff_consumption_class"
-                  >
-                    3. Група Споживання:
-                  </span>
-                </td>
-                <td>
-                  <div className="d-flex">
-                    <div className="volt-form-radio">
-                      <input
-                        type="radio"
-                        id="consumption_group_a"
-                        name="radio-group"
-                      />
-                      <label htmlFor="consumption_group_a">Група А</label>
-                    </div>
-                    <div className="volt-form-radio">
-                      <input
-                        type="radio"
-                        id="consumption_group_b"
-                        name="radio-group"
-                      />
-                      <label htmlFor="consumption_group_b">Група Б</label>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="ff_address">
-                    4. Мiсце пiдключення
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="ff_address"
-                    name="connection_address"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Область, регiон, населений пункт, адреса"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label className="volt-form-label" htmlFor="who_is_provider">
-                    5. Хто є Постачальником електроенергiї
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="who_is_provider"
-                    name="who_is_provider"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Назва постачальника"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="who_is_distributor"
-                  >
-                    6. Хто е Розподiльником електроенергії
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="who_is_distributor"
-                    name="who_is_distributor"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Назва розподiльника"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="price_for_3_month"
-                  >
-                    7. Вартiсть 1 кВт/год за останнi 3 (три) мiсяцi, на момент
-                    подання iнформації
-                  </label>
-                </td>
-                <td>
-                  <input
-                    id="price_for_3_month"
-                    name="price_for_3_month"
-                    className="volt-input"
-                    type="text"
-                    placeholder="Вартість в гривні"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label
-                    className="volt-form-label"
-                    htmlFor="schedulle_payment"
-                  >
-                    8. Графiк оплати за використану е/е:
-                  </label>
-                </td>
-                <td>
-                  <div className="grid-x grid-margin-x">
-                    <div className="cell medium-6">
-                      <input
-                        id="schedulle_payment"
-                        name="schedulle_payment"
-                        className="volt-input"
-                        type="text"
-                        placeholder="Дiючий графiк"
-                      />
-                    </div>
-                    <div className="cell medium-6">
-                      <input
-                        id="schedulle_payment_new"
-                        name="schedulle_payment_new"
-                        className="volt-input"
-                        type="text"
-                        placeholder="Бажаний графiк"
-                      />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </table>
-            <div className="main-form__footer">
-              <div className="d-flex flex-center">
-                <div className="volt-form-checkbox">
-                  <input type="checkbox" id="check_personal_data" />
-                  <label htmlFor="check_personal_data">
-                    <div className="tick"></div>
-                  </label>
-                </div>
-                <label
-                  htmlFor="check_personal_data"
-                  className="volt-form-label c-pointer"
-                >
-                  Погоджуюсь на обробку персональних даних
-                </label>
-              </div>
-              <button
-                className="main-form__submit volt-button volt-button--large"
-                type="submit"
-              >
-                Розрахувати комерційну пропозицію
-              </button>
-              <p className="main-form__small-text">
-                За додатковою інформацією звертайтесь до
-                <a href="#">відділу продажів</a> або в <a href="#">FAQ</a>
-              </p>
-            </div>
-          </form>
+          <ElecticityForm isActive={activeTab === 0} />
+          <GasForm isActive={activeTab === 1} />
         </div>
       </div>
     </section>
